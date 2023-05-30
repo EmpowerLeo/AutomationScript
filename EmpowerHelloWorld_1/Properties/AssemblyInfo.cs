ce81fa5f9bb88c25b4983001380b81dd792abd1c
lgoncalves <lgoncalves@SVT-WS-37>
2023-05-30 15:54:48 -0300

    Adicionar arquivos de projeto.

---
+++ EmpowerHelloWorld_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHelloWorld_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHelloWorld_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5DDE91E4-A234-45D9-9A56-1951B10BFB32")]
+[assembly: AssemblyVersion("1.0.0.0")]
